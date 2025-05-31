4de8a40c4ab03bddb3566b4477f8c9629475f499
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 11:42:18 -0400

    Creadas operaciones con operadores

--- src/operadores/asignacion/OperadoresAsignacion.java
+++ src/operadores/asignacion/OperadoresAsignacion.java
@@ -1,20 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package operadores.asignacion;
 
-/**
- *
- * @author crist
- */
 public class OperadoresAsignacion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Asignaciones simples 
+        /*
+        int a = 5
+        int b = 6;
+        
+        a = b;
+        
+        System.out.println("a = " + a);
+        */
+        
+        // Asignaciones de operación simples
+        /*
+        int a = 5, b = 10, c = 3, d = 8;
+        
+        a = a + 4;
+        b = b - 5;
+        c = c * 3;
+        d = d / 4;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        */
+        
+        // Asignaciones de operación compuestas 
+        int a = 5, b = 10, c = 3, d = 8;
+        
+        a += 4;
+        b -= 5;
+        c *= 3;
+        d /= 4;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
     }
     
 }
